acccabb9021ed941c9933c026be3e064ee1cc286
indri fazliji <93227926+indrifazliji@users.noreply.github.com>
2024-12-14 20:13:20 +0100

    added  User.js model

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,24 @@
 
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  surname: String,
+  country: String,
+  phone: String,
+  studentStatus: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['freelancer', 'client'], required: true },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+ 
+  profilePhoto: { type: String },  
+  workPhotos: [{ type: String }],  
+  description: { type: String },
+  tags: { type: String }
+});
+
+module.exports = mongoose.model('User', UserSchema);
