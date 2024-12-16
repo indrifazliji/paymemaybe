6a3a2e2f0f5b7e4e54cc5f38ad23243c7290fa2d
indri fazliji <93227926+indrifazliji@users.noreply.github.com>
2024-12-16 17:43:51 +0100

    started LandingPage.js

--- client/client/src/components/LandingPage.js
+++ client/client/src/components/LandingPage.js
@@ -1,1 +1,15 @@
+import React from 'react';
 
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      {/* Navbar */}
+      <nav className="navbar">
+        <h1 className="logo">PayMeMaybe</h1>
+        <div className="nav-links">
+          <a href="/login" className="nav-button">Login</a>
+          <a href="/signup" className="nav-button">Sign Up</a>
+        </div>
+      </nav>
+//arsim continue with hero section  or smth else  then gazmend with the footer and finishin it 
